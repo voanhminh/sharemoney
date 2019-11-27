97abd72a0114cdfe4ae301b1e42ec54fd0f087a6
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-11-27 12:52:52 +0700

    ok

--- server.js
+++ server.js
@@ -1,22 +1,6 @@
-// var app = require('./app');
-// var port = process.env.PORT || 3000;
-
-// var server = app.listen(port, function() {
-//   console.log('Express server listening on port ' + port);
-// });
-
-var http = require('http'),
-    fs = require('fs');
-
+var app = require('./app');
 var port = process.env.PORT || 3000;
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(port);
+var server = app.listen(port, function() {
+  console.log('Express server listening on port ' + port);
 });
--- user/Health.js
+++ user/Health.js
@@ -5,9 +5,20 @@ var bodyParser = require('body-parser');
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+
+var http = require('http'),
+    fs = require('fs');
+
 // RETURNS ALL THE USERS IN THE DATABASE
 router.get('/health', function (req, res) {
-    res.status(200).send('Status -> OK');
+    fs.readFile('./index.html', function (err, html) {
+        if (err) {
+            throw err; 
+        }       
+        res.writeHeader(200, {"Content-Type": "text/html"});  
+        res.write(html);  
+        res.end(); 
+    });
 });
 
 module.exports = router;
