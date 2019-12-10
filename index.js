540d8c0dabed2b7286008247db320df28d91c14e
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 16:17:38 +0700

    add index.html

--- index.js
+++ index.js
@@ -11,8 +11,20 @@ app.get('/', (request, response) => {
   response.send('Hello from Express!')
 })
 
-app.get('/users', (request, response) => {
-  response.send('Users!');
+app.get('/users', (request, res) => {
+  try {
+    pool.query('SELECT * FROM public.users', (err, result) => {
+      console.log(err, res)
+      res.statusCode = 200;
+      res.end(`Hello NodeJs! from PORT ${port}\n data: ${JSON.stringify(result || {})}`);
+    })
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 200;
+    res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+  } finally {
+    pool.end();
+  }
 })
 
 app.listen(port, (err) => {
