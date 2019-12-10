188ca2dbdb6bd8b5973842b3a15df91cd02b0559
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 14:33:56 +0700

    change database to todo

--- index.js
+++ index.js
@@ -2,24 +2,41 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const db = require('./db_connect');
+const dbConfig = require('./config/db');
+
+const { Pool, Client } = require('pg');
+const pool = new Pool(dbConfig);
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
 
 const server = http.createServer((req, res) => {
   try {
-    db.getAll('todo')
-      .then(ressult => {
-        res.statusCode = 200;
-        res.end(`Hello Node! from PORT ${port}\n data: ${JSON.stringify(ressult || {})}`);
-      })
-      .catch(e => {
-        console.log(e);
-        res.statusCode = 200;
-        res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
-      })
+    pool.query('SELECT NOW()', (err, res) => {
+      console.log(err, res)
+      res.statusCode = 200;
+      res.end(`Hello Node! from PORT ${port}\n data: ${JSON.stringify(ressult || {})}`);
+    })
+
+    // db.getAll('todo')
+    //   .then(ressult => {
+    //     res.statusCode = 200;
+    //     res.end(`Hello Node! from PORT ${port}\n data: ${JSON.stringify(ressult || {})}`);
+    //   })
+    //   .catch(e => {
+    //     console.log(e);
+    //     res.statusCode = 200;
+    //     res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+    //   })
 
   } catch (e) {
     console.log(e);
     res.statusCode = 200;
     res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+  } finally {
+    pool.end();
   }
 });
 
