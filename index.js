937eceaaa2bf9608f46caaa339e5671ac5217ff3
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 16:15:24 +0700

    add index.html

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const db = require('./db_connect');
+const dbConfig = require('./config/db');
+const { Pool, Client } = require('pg');
+const pool = new Pool(dbConfig);
+
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
+})
+
+app.get('/users', (request, response) => {
+  response.send('Users!');
 })
 
 app.listen(port, (err) => {
