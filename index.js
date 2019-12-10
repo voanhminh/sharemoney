a62031cd87be5eb0c8dedbf4c6fa0c9addf4bf77
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 16:10:51 +0700

    add index.html

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
   response.send('Hello from Express!')
