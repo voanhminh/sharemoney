acfdd1352c787b81e847d94e2c51ff3df514cfed
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 12:20:58 +0700

    add simple node

--- index.js
+++ index.js
@@ -4,17 +4,23 @@ const port = process.env.PORT || 3000;
 const db = require('./db_connect');
 
 const server = http.createServer((req, res) => {
-  db.getAll('todo')
-    .then(ressult => {
-      res.statusCode = 200;
-      res.end(`Hello Node! from PORT ${port}\n data: ${JSON.stringify(ressult || {})}`);
-    })
-    .catch(e => {
-      console.log(e);
-      res.statusCode = 200;
-      res.end(`Hello Node! from PORT ${port}\n`);
-    })
+  try {
+    db.getAll('todo')
+      .then(ressult => {
+        res.statusCode = 200;
+        res.end(`Hello Node! from PORT ${port}\n data: ${JSON.stringify(ressult || {})}`);
+      })
+      .catch(e => {
+        console.log(e);
+        res.statusCode = 200;
+        res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+      })
 
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 200;
+    res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+  }
 });
 
 server.listen(port, () => {
