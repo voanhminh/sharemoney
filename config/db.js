ec1e39724e0aae7ba648d3fb6e54ad7ad5e57ac3
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-10 13:33:53 +0700

    add simple node

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ module.exports = {
     database: 'postgres',
     host: 'database-share-money.c4ybiljgernl.us-east-2.rds.amazonaws.com',
     port: '5432',
-    user: 'master',
+    user: 'postgres',
     password: 'Admin-123',
 }
