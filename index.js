38bc629ffa97a59e84d4976087f88175ef01a5d1
voanhminh27071986@gmail.com <voanhminh27071986@gmail.com>
2019-12-19 11:23:31 +0700

    add google authe

--- index.js
+++ index.js
@@ -7,11 +7,41 @@ const dbConfig = require('./config/db');
 const { Pool, Client } = require('pg');
 const pool = new Pool(dbConfig);
 
+const { OAuth2Client } = require('google-auth-library');
+
+const fs = require('fs');
+
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+app.post('/login', async (req, res) => {
+  try {
+    let body = req.body;
+    let rawGoogleClientCredentials = fs.readFileSync('credentials.json');
+    let googleClientCredentials = JSON.parse(rawGoogleClientCredentials);
+    let CLIENT_ID = googleClientCredentials.web.client_id;
+    let ID_TOKEN = body.id_token;
+
+    const client = new OAuth2Client(CLIENT_ID);
+    const ticket = await client.verifyIdToken({
+      idToken: ID_TOKEN,
+      audience: CLIENT_ID
+    });
+    const payload = ticket.getPayload();
+    const userid = payload['sub'];
+    verify().catch(console.error);
+    res.statusCode = 200;
+    res.json({ rows: result.rows || [], fields: result.fields || [] });
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 500;
+    res.end(`Hello Node! from PORT ${port}\n Error: ${e.message}`);
+  }
+})
 
 app.get('/', (request, res) => {
   //res.send('Hello from Express!');
